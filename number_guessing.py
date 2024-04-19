24b39f68c6c1e0640e7f9ffdf93bf4fb68fe7347
watchman_eugenius <eugenius4real@gmail.com>
2024-04-19 11:17:53 +0100

    number_guessing.py

---
+++ number_guessing.py
@@ -0,0 +1,26 @@
+# Generate random number
+secret = random.randint(1, 50)
+
+# Set guess to zero
+guess = 0
+
+# Set trials to zero
+trials = 0
+
+# Set while loop
+while guess != secret and trials < 5:
+
+    guess = int(input("Enter your guess: "))
+
+    if guess < secret:
+        print("Your guess is too low")
+    elif guess > secret:
+        print("Your guess is too high.")
+
+    trials += 1
+
+# if guess == secret --> you won
+if guess == secret:
+    print("You won! It took you ", trials, " trials to guess that number. ")
+else:
+    print("Oops...You lost ! Better luck next time.")
